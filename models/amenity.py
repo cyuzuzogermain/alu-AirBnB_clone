081ee40d55246a4c1a6061dcd6c8af43129ac065
cyuzuzogermain <c.germain@alustudent.com>
2026-01-11 13:18:22 +0200

    amenities instance

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """_summary_
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    name = ""
